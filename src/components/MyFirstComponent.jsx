8d8cc5a51b73e65a29c723395ab716c0c36663ee
jrojas931022 <jjru931022@gmail.com>
2024-09-17 20:08:16 -0500

    My first component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,18 @@
+
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+    <div>MyFirstComponent</div>
+        <h1>Temas De React</h1>
+        <ul>
+            <li>Componentes</li>
+            <li>Eventos</li>
+            <li>Estados Hooks</li>
+            <li>Props</li>
+            
+        </ul>
+    </>
+  )
+
+}
